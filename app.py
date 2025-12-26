2b8df1f428e076ca6e79ad9baa90a758730e2380
Mohamed Hanafy <m_hanafy@Macdonald.local>
2025-12-26 18:42:27 +0200

    Update project (replace old version)

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <html>
-        <body style="background-color: #F91717FF;">
-            <h1 style="color: red;">CI/CD Pipeline is Working </h1>
-            <p style="font-size: 20px;">Flask app running on</p>
-        </body>
-    </html>
-    """
+    return render_template("main.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+@app.route("/super")
+def super_page():
+    return render_template("super.html")
+
+@app.route("/suv")
+def suv_page():
+    return render_template("suv.html")
+
+@app.route("/mini")
+def mini_page():
+    return render_template("mini.html")
+
+@app.route("/seddan")
+def seddan_page():
+    return render_template("seddan.html")
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
